dc5c54b75fcced444787b7ecba1b3b1789d817f6
KT64 <alecblkly@protonmail.com>
2021-10-29 00:40:22 -0400

    Added .env file for Token.

--- src/main/java/com/kt64/jambotv2/Main.java
+++ src/main/java/com/kt64/jambotv2/Main.java
@@ -1,4 +1,28 @@
 package com.kt64.jambotv2;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+
+import javax.security.auth.login.LoginException;
+
 public class Main {
+
+
+    private static JDABuilder jdaBuilder;
+
+    public static void main(String[] args) {
+
+        jdaBuilder.setToken(Config.get("DISCORD_TOKEN"));
+
+        jdaBuilder.setStatus(OnlineStatus.IDLE);
+        jdaBuilder.setActivity(Activity.playing("IntelliJ"));
+
+        try {
+            jdaBuilder.build();
+        } catch (LoginException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
