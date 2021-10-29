b9f63c2b9474157e88d3c91cc015b31daf63fce5
KT64 <alecblkly@protonmail.com>
2021-10-29 02:01:29 -0400

    Can mention users, bots cannot be mentioned.

--- src/main/java/com/kt64/jambotv2/Main.java
+++ src/main/java/com/kt64/jambotv2/Main.java
@@ -5,12 +5,17 @@ import config.ConfigValues;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
 
-public class Main {
+public class Main extends ListenerAdapter {
 
     private static JDABuilder jdaBuilder;
+    private GuildMessageReceivedEvent event;
 
     public static void main(String[] args) {
 
@@ -22,10 +27,25 @@ public class Main {
         jdaBuilder.setStatus(OnlineStatus.IDLE);
         jdaBuilder.setActivity(Activity.playing("IntelliJ"));
 
+        jdaBuilder.addEventListeners(new Main());
+
         try {
             jdaBuilder.build();
         } catch (LoginException exception) {
             exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event){
+        String[] arguments = event.getMessage().getContentRaw().split(" ");
+        if (arguments[0].equals("!mention")){
+            Member member = event.getMessage().getMentionedMembers().get(0);
+            if (!member.getUser().isBot()){
+                event.getChannel().sendMessage("You've mentioned the person" + member.getUser().getAsMention()).queue();
+            } else {
+                event.getChannel().sendMessage("You tried to hit up a bot, that can't happen").queue();
+            }
         }
     }
 }
