08105dde8899eee70a76acbb5878fefca239728f
KT64 <alecblkly@protonmail.com>
2021-10-29 22:42:39 -0400

    Dylan dungeon command, for lulz.

--- src/main/java/com/kt64/jambotv2/Main.java
+++ src/main/java/com/kt64/jambotv2/Main.java
@@ -2,17 +2,20 @@ package com.kt64.jambotv2;
 
 import config.ConfigFile;
 import config.ConfigValues;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateNicknameEvent;
 import net.dv8tion.jda.api.events.guild.update.GuildUpdateNameEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
+import java.awt.*;
 
 public class Main extends ListenerAdapter {
 
@@ -40,24 +43,17 @@ public class Main extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event){
-        String[] arguments = event.getMessage().getContentRaw().split(" ");
-        if (arguments[0].equals("!mention")){
-            Member member = event.getMessage().getMentionedMembers().get(0);
-            if (!member.getUser().isBot()){
-                event.getChannel().sendMessage("You've mentioned the person" + member.getUser().getAsMention()).queue();
-            } else {
-                event.getChannel().sendMessage("You tried to hit up a bot, that can't happen").queue();
-            }
-        }
-    }
+        String arguments = event.getMessage().getContentRaw();
+        if(arguments.equals("!dylan")) {
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setTitle("Dylan Dungeon Now");
+            embedBuilder.setColor(0x2ecc71);
+            embedBuilder.setDescription(("Get Dylan to the dungeon\nThis is not a drill."));
+            embedBuilder.setFooter("Dylan to dungeon, stat.", event.getAuthor().getAvatarUrl());
+            embedBuilder.setThumbnail(event.getAuthor().getAvatarUrl());
+            embedBuilder.setImage(event.getAuthor().getAvatarUrl());
 
-    @Override
-    public void onGuildUpdateName(@NotNull GuildUpdateNameEvent event) {
-        TextChannel textChannel = event.getGuild().getSystemChannel();
-        if (textChannel != null ) {
-            String newName = event.getNewName();
-            String oldName = event.getOldName();
-            textChannel.sendMessage("The discord name was succesfully changed from **" + oldName + "** to **" + newName + "**!").queue();
+            event.getChannel().sendMessage(embedBuilder.build()).queue();
         }
     }
 }
